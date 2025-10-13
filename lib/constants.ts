64ae3cc6846c369e02ab576780cda8ac19b51844
Anderson Burgers <burgerlivesmatter@gmail.com>
2025-10-13 16:12:59 -0400

    v2

---
+++ lib/constants.ts
@@ -0,0 +1,8 @@
+export const NAV_ITEMS: {href: string; label: string}[] = [
+    {href: '/', label: 'Dashboard' },
+    {href: '/search', label: 'Search' },
+    {href: '/watchlist', label: 'Watchlist' },
+    {href: '/posCalculator', label: 'PositionCalculator' },
+
+
+]
