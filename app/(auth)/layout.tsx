8531a61adcb6a10460e0dea54bed42828ce9bc00
Edwin Lin <burgerlivesmatter@gmail.com>
2025-11-20 10:18:12 -0500

    updated widgets

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,23 @@
 import Header from "@/components/Header";
-
-
+import Link from "next/link";
+import Image from "next/image";
+import TradingLogo from "@publi";
 const Layout = ({ children }: { children: React.ReactNode }) => {
-    return (
-        <main className="min-h-screen text-gray-400">
-        
-        </main>
-    )
-}
-export default Layout
+  return (
+    <main className="auth-layout ">
+      <section className="auth-left-section scrollbar-hide-default">
+        <Link href="/" className="auth-logo">
+          <Image
+            placeholder="blur"
+            src="/assets/icons/discordtrading.png"
+            alt="Buy-Side-Liquidity"
+            width={140}
+            height={32}
+            className="h-8 w-auto"
+          />
+        </Link>
+      </section>
+    </main>
+  );
+};
+export default Layout;
