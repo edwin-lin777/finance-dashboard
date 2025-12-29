b318c17cc6bd06e5d4b2e5877dade8346df42cf1
Edwin Lin <burgerlivesmatter@gmail.com>
2025-12-28 20:51:57 -0800

    fix

--- app/(auth)/reset-password/page.tsx
+++ app/(auth)/reset-password/page.tsx
@@ -1,5 +1,3 @@
-export const dynamic = 'force-dynamic';
-export const runtime = 'edge'; // Optional, but helps with static generation
 "use client"
 import React from 'react'
 import { useRouter, useSearchParams } from 'next/navigation'
